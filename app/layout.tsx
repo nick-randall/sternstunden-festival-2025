25f575b051edb941d9be11903a7808607f07a685
rabotnik1 <randall@posteo.de>
2025-03-04 15:38:52 +1000

    added @media css rules as inline style tag -- to fix them for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,46 @@ import "../styles/background.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Sternstunden Festival 2025</title>
+          <style>
+          {
+            `@media screen and (max-width: 1080px) {
+              .content-box {
+                flex-direction: column;
+              }
+              .content-box .responsive-spacer-15 {
+                height: 15px;
+              }
+            }
+            @media screen and (max-width: 768px) {
+              .footer {
+                flex-direction: column;
+                align-items: center;
+              }
+            }  
+            @media screen and (max-width: 768px) {
+              .header-logo {
+                height: 300px;
+              }
+              .footer-logo {
+                height: 150px;
+              }
+            }   
+            `}
+  </style>
+      </head>
+      
       <body>
-       
-          <div className="bg-distortion">
+        <div className="bg-distortion">
           <div className="bg-gradient">
             <div className="bg-triangles">
               <div className="page-wrapper">
-              <img className="header-logo" src="logo-with-dates.png" alt="Sternstunden Festival Logo" />
+                <img className="header-logo" src="logo-with-dates.png" alt="Sternstunden Festival Logo" />
 
-                {children}</div>
+                {children}
+              </div>
             </div>
           </div>
         </div>
