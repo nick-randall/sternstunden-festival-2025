976c63d64a1c88a55b86f27aa576c334aa189dae
rabotnik1 <randall@posteo.de>
2025-03-21 11:00:10 +1000

    scroll arrow logic mostly working

--- app/MobilePhotoSlider.tsx
+++ app/MobilePhotoSlider.tsx
@@ -1,37 +1,42 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
 interface MobilePhotoSliderProps {
   photoUrls: string[];
 }
+interface WidthMap {
+  [url: string]: number;
+}
 
 const MobilePhotoSlider: React.FC<MobilePhotoSliderProps> = ({ photoUrls }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [userScrolling, setUserScrolling] = useState(false);
+  const [widthMap, setWidthMap] = useState<WidthMap>({});
 
-  // const pauseScroll = () => {
-  //   setUserScrolling(true);
-  // };
+  const pauseScroll = useCallback(() => {
+    setUserScrolling(true);
+    setTimeout(() => setUserScrolling(false), 3000);
+  }, []);
 
   // const resumeScroll = () => {
   //   setUserScrolling(false);
   // };
 
-  useEffect(() => {
-    const stopAutoScroll = () => {
-      setUserScrolling(true);
-      setTimeout(() => setUserScrolling(false), 3000);
-    };
-    const container = containerRef.current;
-    if (!container) return;
-    container.addEventListener("wheel", stopAutoScroll);
-    container.addEventListener("touchstart", stopAutoScroll);
-    return () => {
-      container.removeEventListener("wheel", stopAutoScroll);
-      container.removeEventListener("touchstart", stopAutoScroll);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const stopAutoScroll = () => {
+  //     setUserScrolling(true);
+  //     setTimeout(() => setUserScrolling(false), 3000);
+  //   };
+  //   const container = containerRef.current;
+  //   if (!container) return;
+  //   container.addEventListener("wheel", stopAutoScroll);
+  //   container.addEventListener("touchstart", stopAutoScroll);
+  //   return () => {
+  //     container.removeEventListener("wheel", stopAutoScroll);
+  //     container.removeEventListener("touchstart", stopAutoScroll);
+  //   };
+  // }, []);
 
   useEffect(() => {
     const container = containerRef.current;
@@ -59,11 +64,20 @@ const MobilePhotoSlider: React.FC<MobilePhotoSliderProps> = ({ photoUrls }) => {
     } else console.log("scroll too high. must be at end of images");
   };
 
-  const getLeftVals = () => {
+  const scrollBackward = () => {
     const container = containerRef.current;
-    if (!container) return [];
-    const children = Array.from(container.children) as HTMLImageElement[];
-    const widths = children.map(child => child.width);
+    if (!container) return;
+    const currImageIdx = findCurrImageIdx(container.scrollLeft);
+    if (currImageIdx < photoUrls.length - 1) {
+      const scrollTo = getLeftVals()[currImageIdx - 1];
+
+      container.scrollTo({ left: scrollTo, behavior: "smooth" });
+    } else console.log("scroll too high. must be at end of images");
+  };
+  const margin = 10;
+
+  const getLeftVals = () => {
+    const widths = photoUrls.map(url => widthMap[url] + margin);
     const lefts = widths.reduce((acc, curr) => [...acc, acc[acc.length - 1] + curr], [0]);
     return lefts;
   };
@@ -82,20 +96,15 @@ const MobilePhotoSlider: React.FC<MobilePhotoSliderProps> = ({ photoUrls }) => {
     return closestIdx;
   }
 
-  useEffect(() => {
-    const pauseScroll = () => {
-      setUserScrolling(true);
-      setTimeout(() => setUserScrolling(false), 3000);
-    };
-    const container = containerRef.current;
-    if (!container) return;
-    container.addEventListener("wheel", pauseScroll);
-    container.addEventListener("touchstart", pauseScroll);
-    return () => {
-      container.removeEventListener("wheel", pauseScroll);
-      container.removeEventListener("touchstart", pauseScroll);
-    };
-  });
+  const handleRef = useCallback((node: HTMLImageElement | null) => {
+    if (node) {
+      node.onload = () => {
+        if (!node.width) return;
+        setWidthMap(prev => ({ ...prev, [node.src]: node.width }));
+      };
+    }
+  }, []);
+
   return (
     <>
       <div
@@ -109,13 +118,13 @@ const MobilePhotoSlider: React.FC<MobilePhotoSliderProps> = ({ photoUrls }) => {
       >
         <div style={{ position: "relative" }}>
           <Image
-            onClick={scrollForward}
+            onClick={scrollBackward}
             src="./chevron-right.svg"
             alt=""
-            className="arrow arrow-right"
+            className="arrow arrow-left"
             height="50"
             width="50"
-            style={{ position: "absolute", height: 50, left: 0, top: 200, transform: "translate(-0%, -50%) rotateY(180deg)", pointerEvents: "auto" }}
+            // style={{ position: "absolute", height: 50, left: 0, top: 200, transform: "translate(-0%, -50%) rotateY(180deg)", pointerEvents: "auto" }}
           />
           <Image
             onClick={scrollForward}
@@ -123,15 +132,15 @@ const MobilePhotoSlider: React.FC<MobilePhotoSliderProps> = ({ photoUrls }) => {
             alt=""
             height="50"
             width="50"
-            className="arrow arrow-right"
-            style={{ position: "absolute", height: 50, right: 0, top: 200, transform: "translate(-0%, -50%)", pointerEvents: "auto" }}
+            className="arrow"
+            // style={{ position: "absolute", height: 50, right: 0, top: 200, transform: "translate(-0%, -50%)", pointerEvents: "auto" }}
           />
         </div>
       </div>
-      <div ref={containerRef} key="mobile-photo-slider" className="photo-slider">
+      <div ref={containerRef} key="mobile-photo-slider" className="photo-slider" onWheel={pauseScroll} onTouchStart={pauseScroll}>
         {photoUrls.map((url, index) => (
           <>
-            <img className="photo" key={index + "image"} src={url} alt="Foto Sternstunde 2025" />
+            <img ref={handleRef} className="photo" key={index + "image"} src={url} alt="Foto Sternstunde 2025" />
             <div key={index + "spacer"} className="img-spacer"></div>
           </>
         ))}
