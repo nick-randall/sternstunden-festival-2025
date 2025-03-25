ba71b3e58eaff8543547c8abf6cd0772ad2f5f38
rabotnik1 <randall@posteo.de>
2025-03-25 17:57:58 +1000

    added click to rotate

--- components/PlanetsSketch.tsx
+++ components/PlanetsSketch.tsx
@@ -1,28 +1,32 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import p5 from "p5";
 
 const Planets: React.FC = () => {
-  "use client"; // Ensures it runs only on the client
 
   const sketchRef = useRef<HTMLDivElement>(null);
-  const planetUrls = [
-    "https://static.wixstatic.com/media/4e9673_ea8d802639134ddb956fb341a28561f4~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_ea8d802639134ddb956fb341a28561f4~mv2.png",
-    "https://static.wixstatic.com/media/4e9673_ea8d802639134ddb956fb341a28561f4~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_ea8d802639134ddb956fb341a28561f4~mv2.png",
-    "https://static.wixstatic.com/media/4e9673_3be9bfb05a8c47078c1f40fb0727f53a~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_3be9bfb05a8c47078c1f40fb0727f53a~mv2.png",
-    "https://static.wixstatic.com/media/4e9673_05721e09679945d78b1ce0756698b739~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_05721e09679945d78b1ce0756698b739~mv2.png",
-    "https://static.wixstatic.com/media/4e9673_d6bf152709c5441096d58cb139b164b9~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_d6bf152709c5441096d58cb139b164b9~mv2.png",
-    "https://static.wixstatic.com/media/4e9673_fe9d7c90cde3486bb3b53a60fd60ae54~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_fe9d7c90cde3486bb3b53a60fd60ae54~mv2.png",
-    "https://static.wixstatic.com/media/4e9673_9c815a65d3b6420c92a08b9031e6ac32~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_9c815a65d3b6420c92a08b9031e6ac32~mv2.png",
-    "https://static.wixstatic.com/media/4e9673_65cf5e1bf9a14c66953c0382b37300c4~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_65cf5e1bf9a14c66953c0382b37300c4~mv2.png",
-    "https://static.wixstatic.com/media/4e9673_58efaa4a95b8408393fc22df21bba084~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_58efaa4a95b8408393fc22df21bba084~mv2.png",
-  ];
+  const planetUrls = useMemo(
+    () => [
+      "https://static.wixstatic.com/media/4e9673_ea8d802639134ddb956fb341a28561f4~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_ea8d802639134ddb956fb341a28561f4~mv2.png",
+      "https://static.wixstatic.com/media/4e9673_86378abffefa418896d694960dcc2139~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_86378abffefa418896d694960dcc2139~mv2.png",
+      "https://static.wixstatic.com/media/4e9673_3be9bfb05a8c47078c1f40fb0727f53a~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_3be9bfb05a8c47078c1f40fb0727f53a~mv2.png",
+      "https://static.wixstatic.com/media/4e9673_05721e09679945d78b1ce0756698b739~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_05721e09679945d78b1ce0756698b739~mv2.png",
+      "https://static.wixstatic.com/media/4e9673_d6bf152709c5441096d58cb139b164b9~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_d6bf152709c5441096d58cb139b164b9~mv2.png",
+      "https://static.wixstatic.com/media/4e9673_fe9d7c90cde3486bb3b53a60fd60ae54~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_fe9d7c90cde3486bb3b53a60fd60ae54~mv2.png",
+      "https://static.wixstatic.com/media/4e9673_9c815a65d3b6420c92a08b9031e6ac32~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_9c815a65d3b6420c92a08b9031e6ac32~mv2.png",
+      "https://static.wixstatic.com/media/4e9673_65cf5e1bf9a14c66953c0382b37300c4~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_65cf5e1bf9a14c66953c0382b37300c4~mv2.png",
+      "https://static.wixstatic.com/media/4e9673_58efaa4a95b8408393fc22df21bba084~mv2.png/v1/fill/w_304,h_314,al_c,q_80,usm_0.66_1.00_0.01/4e9673_58efaa4a95b8408393fc22df21bba084~mv2.png",
+    ],
+    []
+  );
 
   useEffect(() => {
     const numPlanets = planetUrls.length;
     let rotation = 0;
+    let rotateTo: number | undefined = undefined;
     const planetImgs: p5.Image[] = [];
+    const planetSize = 120;
 
     const radiusX = 520; // X-axis stretch
     const radiusY = 190; // Y-axis stretch
@@ -33,44 +37,26 @@ const Planets: React.FC = () => {
             planetImgs.push(img);
           });
         }
-      }
+      };
       p.setup = () => {
-        p.createCanvas(800, 400, p.WEBGL);
-        try {
-          const font = p.loadFont("/NunitoSans.ttf");
-          p.textFont(font);
-        } catch (e) {
-          console.log(e);
-        }
-       
+        p.createCanvas(800, 500, p.WEBGL);
       };
 
       const drawCircle = (index: number) => {
         p.push();
-        // const radius = 100;
         const rads = (Math.PI * 2) / numPlanets;
         const angle = rads * index - rotation;
         const sin = Math.sin(angle); // Stretch orbit along x-axis
         const cos = Math.cos(angle); // Stretch orbit along y-axis
         const y = radiusX * cos;
         const x = radiusY * sin;
-        // const frontThree = (angle > -1 && angle < 0) || (angle < 1 && angle >0);
-        let size = 120;
-        const factor = 1; // Math.abs(Math.PI - angle) / 4;
-        // if (angle > -1 && angle < 0) size *= 1.2;
-        // if (angle < 1 && angle > 0) size *= 1.2;
-        // if (angle > -1 && angle < 0) factor = angle * 2;
-        // if (angle < 1 && angle > 0) factor = angle;
-        size *= factor;
+
         p.translate(x, y, 0);
         p.rotateX(-p.PI / 2 - 0.06);
-        // p.text(`${index} factor:${Math.round(Math.abs(Math.PI - angle))}`, 0, 0);
-        // p.ellipse(0, 0, size, size);
-        // console.log(planetImgs[index]);
-        // p.image(planetImgs[index], 0, 0, size, size);
+
         p.texture(planetImgs[index]);
         p.noStroke();
-        p.circle(0, 0, size);
+        p.circle(0, 0, planetSize);
         p.pop();
       };
 
@@ -82,10 +68,24 @@ const Planets: React.FC = () => {
         for (let i = 0; i < numPlanets; i++) {
           drawCircle(i);
         }
-        // if (rotation > Math.PI) rotation = 0;
-        rotation += 0.01;
+        if (rotation > Math.PI * 2) {
+          rotation = 0;
+        }
+        if (rotateTo && Math.abs(rotateTo - rotation) < 0.01) {
+          rotation = rotateTo;
+          rotateTo = undefined;
+        }
+        if (rotateTo && rotateTo < rotation) {
+          rotation -= 0.01;
+        } else if (rotateTo && rotateTo > rotation) {
+          rotation += 0.01;
+        }
       };
+      p.mouseClicked = () => {
+        rotateTo = rotation + ((Math.PI * 2) / numPlanets);
+      }
     };
+
     if (sketchRef.current === null) return;
 
     const myP5 = new p5(sketch, sketchRef.current);
@@ -93,7 +93,7 @@ const Planets: React.FC = () => {
     return () => {
       myP5.remove(); // Cleanup when unmounting
     };
-  }, []);
+  }, [planetUrls]);
 
   return <div ref={sketchRef} />;
 };
