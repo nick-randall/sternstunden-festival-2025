a1d277851de033f4208347a6dccdedf46ad78971
rabotnik1 <randall@posteo.de>
2025-04-04 12:56:41 +1000

    tested embedding weeztix (eventix) on /tickets page

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,6 +1,7 @@
+import Script from "next/script";
 import "../../styles/common.css";
-const Tickets : React.FC = () => { 
-return (
+const Tickets: React.FC = () => {
+  return (
     <div className="content-box">
       <div className="text-box">
         <div>
@@ -9,17 +10,16 @@ return (
             Tickets sind ab dem 01.09.2024 erhältlich. Der Ticketpreis beträgt 15 Euro.
             <br />
             <br />
-            Das Ticket beinhaltet den Eintritt zu allen Konzerten und Vorträgen, sowie den Zugang zu den
-            Sternenbeobachtungen.
+            Das Ticket beinhaltet den Eintritt zu allen Konzerten und Vorträgen, sowie den Zugang zu den Sternenbeobachtungen.
           </p>
         </div>
         <br />
-        <a className="more-info" href="/info">
-          MEHR INFOS
-        </a>
+        <div id="shop-frame" data-url="https://shop.weeztix.com/eddc7fdc-06c6-11ee-94ba-6a57c78572ab" style={{maxWidth: 600, margin:"0 auto;"}}></div>
+        <Script src="https://shop.weeztix.com/build/integrate.js"></Script>
+
       </div>
     </div>
-)
-}
+  );
+};
 
-export default Tickets; 
+export default Tickets;
