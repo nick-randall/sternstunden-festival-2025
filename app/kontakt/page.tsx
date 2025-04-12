c83978cf6db01a6b72c5ce133577d7960fb26953
rabotnik1 <randall@posteo.de>
2025-04-12 22:41:53 +1000

    contact form working

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,44 +1,103 @@
+"use client";
 import Link from "next/link";
 import "../../styles/common.css";
 import "../../styles/contact-page.css";
 import Spacer from "@/components/Spacer";
+import { useState } from "react";
+
+interface ContactFormValues {
+  firstname: string;
+  lastname: string;
+  email: string;
+  message: string;
+}
+
 const KontaktPage: React.FC = () => {
+  const [formValues, setFormValues] = useState<ContactFormValues>({
+    firstname: "",
+    lastname: "",
+    email: "",
+    message: "",
+  });
+  const [sending, setSending] = useState(false);
+  const [sent, setSent] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormValues(prevValues => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    if(sending) return;
+    setSending(true);
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted:", formValues);
+    window.history.pushState({}, document.title);
+    handleSend();
+  };
+
+  const handleSend = async () => {
+   try{ const resp = await fetch("https://sternstunde.fly.dev/send-email", { method: "POST", body: JSON.stringify(formValues) });
+    const status = resp.status;
+    if (status === 200) {
+      setFormValues({ firstname: "", lastname: "", email: "", message: "" });
+    }
+    if (status === 404) {
+      alert("Es gab ein Problem beim Senden der Nachricht. Bitte versuchen Sie es später erneut.");
+      setErrorMessage("Es gab ein Problem beim Senden der Nachricht. Bitte versuchen Sie es später erneut.");
+    }}
+    catch (error) {
+      console.error("Error sending email:", error);
+      alert("Es gab ein Problem beim Senden der Nachricht. Bitte versuchen Sie es später erneut.");
+      setErrorMessage("Es gab ein Problem beim Senden der Nachricht. Bitte versuchen Sie es später erneut.");
+    }
+    setSending(false);
+    setSent(true);
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <div className="contact-form-container">
         <h1>KONTAKT</h1>
         <Link href="mailto:info@sternstundenfestival.de">info@sternstundenfestival.de</Link>
-        {/* <Flex width="100%" justifyContent="center" alignItems="center" flexGrow="1"> */}
         <Spacer height={16} />
         <div className="contact-form-row">
           <div className="item">
-            <label htmlFor="first-name">Vorname</label>
-            <input type="text" />
+            <label htmlFor="firstname">Vorname</label>
+            <input name="firstname" type="text" value={formValues.firstname} onChange={handleChange} />
           </div>
           <div className="item">
-            <label htmlFor="first-name">Nachname</label>
-            <input type="text" />
+            <label htmlFor="lastname">Nachname</label>
+            <input name="lastname" type="text" value={formValues.lastname} onChange={handleChange} />
           </div>
         </div>
         <div className="contact-form-row">
           <div className="item">
-            <label htmlFor="first-name">Email-Adresse *</label>
-            <input type="text" />
+            <label htmlFor="email">Email-Adresse *</label>
+            <input name="email" type="email" required value={formValues.email} onChange={handleChange} />
           </div>
         </div>
         <div className="contact-form-row">
           <div className="item">
-            <label htmlFor="first-name">Nachricht Schreiben</label>
-            <textarea className="message" />
+            <label htmlFor="message">Nachricht Schreiben</label>
+            <textarea name="message" className="message" value={formValues.message} onChange={handleChange} />
           </div>
         </div>
         <div className="contact-form-row">
           <div className="item">
-            <button>Absenden</button>
+            {sent && errorMessage === "" ? <div>Erfolgreich gesendet!</div> : <button type="submit">{sending ? "Wird abgesendet..." : "Absenden"}</button>}
           </div>
+        </div>
+        <div className="contact-form-row">
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
         </div>
       </div>
-    </>
+    </form>
   );
 };
 
