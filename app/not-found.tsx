b87b83206fbafc0ec85a3010d4b36693e4dcc793
rabotnik1 <randall@posteo.de>
2025-04-20 21:54:43 +1000

    added spaceman for 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import '../styles/common.css';
+import Image from "next/image";
 
 const NotFoundPage = () => {
-  //Auf deutsch bitte
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+      <Image src="/404.png" alt="Austronaut mit 404 Seite nicht gefunden" height="1029" width="607" style={{height: "60%", width:"auto"}}/>
+      <div style={{height: 30}}></div>
+      <Link href="/">Zurück zur Startseite</Link>
     </div>
   );
 };
