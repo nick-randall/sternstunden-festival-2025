b741441dfd07d81a5e10aa90d616bc04b7a7ff1f
baebop <jakobtimm@gmx.de>
2025-04-22 13:31:30 +0200

    support us layout changes

--- app/support-us/page.tsx
+++ app/support-us/page.tsx
@@ -9,8 +9,7 @@ const SupportUs = () => {
             <h1>Sponsoring</h1>
 
             <h3> Sponsor werden? Sehr gern!</h3>
-            <br/>
-
+            
             Das Sternstunden Festival lebt von Engagement – auch von Partner:innen aus Wirtschaft und Gesellschaft.
             Ob mit finanzieller Unterstützung oder Sachleistungen wie Technik, Materialien oder Verpflegung:
             Jede Form von Sponsoring hilft uns, Kultur und Wissenschaft für alle zugänglich zu machen.
@@ -21,26 +20,29 @@ const SupportUs = () => {
             Unternehmen können Förderbeträge von der Steuer absetzen!<br/>
             <br/>
             <strong> Jetzt ansprechen & Teil des Festivals werden!</strong>
-
+            <br/>
             <h1>Spenden</h1>
 
             <h3>Auch Privatleute können uns unterstützen!</h3>
+            
             Als <strong> ehrenamtlich organisierte Non-Profit-Veranstaltung</strong> sind wir auf  <strong> gemeinschaftliche Unterstützung</strong>  angewiesen, um unser Festival möglich zu machen.
             Das Sternstunden Festival kannst du auch  mit einer Spende als Privatperson fördern. So leistest du deinen Beitrag, die Kultur und Wissenschaft Hamburgs abseits des Mainstreams zu bereichern.
             Wenn du uns unter die Arme greifen möchtest, freuen wir uns sehr über deine Spende auf folgendes Konto:
-
-            Förderverein der Unimusik Hamburg e.V.
-            DE39 8306 5408 0004 1718 96
-            GENODEF1SLR
-            Deutsche Skatbank
-            
+            <br/>
+            <br/>
+            Förderverein der Unimusik Hamburg e.V.<br/>
+            DE39 8306 5408 0004 1718 96<br/>
+            GENODEF1SLR<br/>
+            Deutsche Skatbank<br/>
+            <br/>
             Eine Spendenbescheinigung können wir auf Wunsch ausstellen. Bitte kontaktiere uns dafür unter:  <a href="mailto:info@foerderverein-unimusik-hamburg.de?subject=Spendenbescheinigung%20Sternstunden">
             <strong>info@foerderverein-unimusik-hamburg.de</strong></a> mit dem Betreff <strong> “Spendenbescheinigung Sternstunden”</strong>.<br/>
             
-
+            <br/>
             <h1>Helfen</h1>
 
             <h3>Hilf uns beim Festival!</h3>
+            
             Wir möchten unsere Verbindung zu unseren Nachbar:innen stärken.
             Hast du Lust uns zu unterstützen und dich für ein Projekt in deiner Nachbarschaft einzusetzen?
             Sprich uns gerne an!<br/>
