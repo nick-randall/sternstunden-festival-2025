0a6de8254303db54547984cbd035c2ac3223e5b7
Jakob Timm <jakobtimm@MacBook-Pro-von-Jakob.local>
2025-04-22 12:40:16 +0200

    test

--- app/mission-statement/page.tsx
+++ app/mission-statement/page.tsx
@@ -9,6 +9,7 @@ const MissionStatement = () => {
             <h1>Mission Statement</h1>
             Wir verstehen uns als spartenübergreifendes Kulturfestival, mit dem wir eine Brücke insbesondere zwischen
             unterschiedlichen Musikgenres und Astrophysik schlagen möchten.
+            
             So sollen Ideen und Gedanken miteinander verknüpft und präsentiert werden. Ungehörtes, Ungesehenes erlaubt uns,
             die Welt aus neuen Perspektiven zu betrachten, Gedankenexperimente zu wagen.
             Wir wollen Gemeinschaften stärken, kulturelle Barrieren überwinden und ein bereicherndes Erlebnis im Hier und Jetzt für alle schaffen.
