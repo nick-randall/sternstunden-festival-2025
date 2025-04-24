f12bb2ac2f54c31a79caec57f4da0e6aa08f8d4a
rabotnik1 <randall@posteo.de>
2025-04-24 18:10:42 +1000

    tweak to get collapsible text properly expanding and shrinking

--- components/CollapsibleText.tsx
+++ components/CollapsibleText.tsx
@@ -9,18 +9,19 @@ interface CollapsibleTextProps {
 }
 
 const CollapsibleText: FC<CollapsibleTextProps> = ({ title, text, isOpen, handleClick }) => {
-
   return (
     <div className="collapsible-text">
       <div className="title-box">
         <div>{title}</div>
-        <button className={`plus-minus-button ${isOpen ? "minus": "plus"}`} onClick={handleClick}>
-          <span className={isOpen ? "minus": "plus"}></span>
-          <span className={isOpen ? "minus": "plus"} ></span>
+        <button className={`plus-minus-button ${isOpen ? "minus" : "plus"}`} onClick={handleClick}>
+          <span className={isOpen ? "minus" : "plus"}></span>
+          <span className={isOpen ? "minus" : "plus"}></span>
         </button>
       </div>
-      <div style={{ height: isOpen ? 10 : 0, transition: "0.3s ease" }}></div>
-      <div className={`collapsible-text-box ${isOpen ? "open" : "closed"}`}>{text}</div>
+      <div style={{ height: isOpen ? 10 : 0, transition: "0.3s ease-in-out" }}></div>
+      <div className={`collapsible-text-box ${isOpen ? "open" : "closed"}`}>
+        <div className="text-content">{text}</div>
+      </div>
     </div>
   );
 };
