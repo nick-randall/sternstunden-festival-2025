a4239794a868b14559fee955359e526ece5eb0eb
baebop <jakobtimm@gmx.de>
2025-05-01 14:54:40 +0200

    programm platzhalter text

--- app/programm/page.tsx
+++ app/programm/page.tsx
@@ -4,8 +4,21 @@ import "../../styles/common.css";
 const ProgrammPage = () => {
   return (
     <div className="boring-page-wrapper">
-      <h1>Programm Page</h1>
-      <p>This is the program page.</p>
+      <h1>Programm Sternstunden Festival 2025</h1>
+      <h2>Musik trifft Kosmos – und du bist eingeladen.</h2>
+      <p>Klangvolle Nächte, ein historischer Ort, faszinierende Einblicke ins Universum: Im Juli verwandelt sich die Hamburger Sternwarte erneut in ein Festivalgelände der besonderen Art.</p>
+    <p>Das Festivalprogramm für dieses Jahr wird <strong>in Kürze</strong> veröffentlicht. Freu dich auf:
+
+<ul>
+  <li>musikalische Highlights unter freiem Himmel</li>
+  <li>neue Perspektiven auf Astronomie & Wissenschaft</li>
+  <li>ein liebevoll gestaltetes Kinderprogramm</li>
+  <li>inspirierende Begegnungen in einzigartiger Atmosphäre</li>
+</ul>
+</p>
+<h3>Merke dir das Wochenende im Juli vor – und gönn dir eine Sternstunde.</h3>
+<h3>Folge uns auf Instagram und erfahre direkt, wenn das vollständige Programm online ist.</h3>
+<p>Spätestens einen Monat vor Beginn des Festivals wird das vollständige Programm auf unserer Website zur Verfügung gestellt werden.</p>
     </div>
   );
 }
