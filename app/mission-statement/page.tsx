63b11da3bd9fe31b751f322a34a4983a792ee29e
baebop <jakobtimm@gmx.de>
2025-05-14 07:32:42 +0200

    Mission Statement

--- app/mission-statement/page.tsx
+++ app/mission-statement/page.tsx
@@ -1,7 +1,7 @@
 // import Flex from "@/components/Flex";
 import "../../styles/common.css";
 import "../../styles/boring-pages.css";
-
+import Spacer from "@/components/Spacer";
 
 const MissionStatement = () => {
     return (
@@ -14,7 +14,22 @@ const MissionStatement = () => {
             Wir wollen Gemeinschaften stärken, kulturelle Barrieren überwinden und ein bereicherndes Erlebnis im Hier und Jetzt für alle schaffen.
             Willkommen auf dem Sternstunden Festival, wir laden euch ein, die faszinierenden Welten von Musik und Astrophysik mit uns zu entdecken.
             </p>
+           <Spacer height={16} />
+
+             <h2>REGELN</h2>
+            <p>
+            Keine Hunde! <br/>
+            Kein offenes Feuer oder Grillen!<br/>
+            Kein Konfetti!<br/>
+            Diskriminierung aller Art, insbesondere aufgrund von Geschlecht, sexueller Orientierung oder Herkunft hat keinen Platz auf dem Sternstunden Festival. <br/>
+            Respektiert euch gegenseitig, seid freundlich und einvernehmlich. <br/>
+            Sprecht uns an, wenn ihr Hilfe braucht.
+            </p>
+    
         </div>
+
+       
+               
     );
 };
 export default MissionStatement;
