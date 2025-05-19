f81fa45e4681ff7f9a35100bd135cc6762a80d9c
baebop <jakobtimm@gmx.de>
2025-05-19 13:36:16 +0200

    past acts on program page

--- app/programm/page.tsx
+++ app/programm/page.tsx
@@ -1,5 +1,6 @@
 import "../../styles/boring-pages.css";
 import "../../styles/common.css";
+import Spacer from "@/components/Spacer";
 
 const ProgrammPage = () => {
   return (
@@ -20,6 +21,24 @@ const ProgrammPage = () => {
         </ul>
       </p>
       <h3>Folge uns auf Instagram und erfahre direkt, wenn das vollständige Programm online ist.</h3>
+        <Spacer height={32} />
+      <h3>In den letzten Jahren haben unter anderem folgende Acts bei uns gespielt</h3>
+      <p> <a href="https://www.heptapolka.com/" target="_blank">Hepta Polka &#10154;</a><br/>
+          <a href="https://treppenhausorchester.de/" target="_blank">Orchester im Treppenhaus mit DISCO &#10154;</a><br/>
+          <a href="https://www.pabameto.com/" target="_blank">Pabameto &#10154;</a><br/>
+          <a href="https://www.rundfunk.band/" target="_blank">RundFunk Collective &#10154;</a><br/>
+          <a href="https://soundcloud.com/subios/abis-laufer-never-break-the-chain-original-mix" target="_blank">DJ Abis Läufer &#10154;</a><br/>
+          <a href="https://www.justuswolf.com/pepziecarola-1" target="_blank">pepziecarola &#10154;</a><br/>
+          <a href="https://www.pouyaabdimusic.com/" target="_blank">Pouya Abdi &#10154;</a><br/>
+          <a href="https://www.instagram.com/cristelsellliah/" target="_blank">Cristel Sellliah &#10154;</a> <br/>
+          <a href="https://www.quintabulous.de/" target="_blank">Quintabulous &#10154;</a><br/>
+          <a href="https://www.quabla-band.com/de" target="_blank">Quabla &#10154;</a> <br/>
+          <a href="https://ballymoon.bandcamp.com/album/demos-2020" target="_blank">Ballymoon &#10154;</a><br/>
+          <a href="https://www.unimusik.uni-hamburg.de/ensembles/big-band.html" target="_blank">Skyliner Bigband &#10154;</a><br/>
+          <a href="https://www.instagram.com/lucidrobberyofficial/" target="_blank">Lucid Robbery &#10154;</a> <br/>
+          <a href="https://www.instagram.com/bow.brothers/" target="_blank">Bow Brothers &#10154;</a><br/>
+          <a href="https://www.fjcello.eu/" target="_blank">Felix Jedeck &#10154;</a><br/>
+</p>
     </div>
   );
 };
