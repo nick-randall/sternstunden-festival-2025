90f26fb595a966e218da0caf68f41613618fb385
rabotnik1 <randall@posteo.de>
2025-05-22 10:06:40 +1000

    added hidden page to show and test moment app

---
+++ app/hidden/page.tsx
@@ -0,0 +1,12 @@
+const HiddenPage = () => {
+  return (
+    <div>
+      <h1>Hidden Page</h1>
+      <iframe src="https://app.momentevents.co/m/GXzwWJKGYjWEyKpCy9Jw" width="800" height="600"></iframe>
+      <h1>Als Link</h1>
+
+      <a href="https://app.momentevents.co/m/GXzwWJKGYjWEyKpCy9Jw">ZUM PROGRAMM</a>
+    </div>
+  );
+};
+export default HiddenPage;
